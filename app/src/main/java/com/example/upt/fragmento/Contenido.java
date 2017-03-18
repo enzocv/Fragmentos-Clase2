2a8161f27de88cd50e270dfac9bc8bfd724925b2
USERUPT <USERUPT>
2017-03-18 11:59:16 +0100

    first commit -  Clase 2 de Soluciones Móviles I

---
+++ app/src/main/java/com/example/upt/fragmento/Contenido.java
@@ -0,0 +1,19 @@
+package com.example.upt.fragmento;
+
+/**
+ * Created by USERUPT on 18/03/2017.
+ */
+
+public class Contenido {
+
+    static String[] Titulos = {
+            "Primer párrafo",
+            "Segundo párrafo"
+    };
+
+    static String[] Parrafos = {
+            "P1) Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab asperiores consectetur",
+            "P2)Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab asperiores consectetur cumque, distinctio eius, facere, fuga"
+    };
+
+}
